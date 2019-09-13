e85f3ef085bc3989987bf35521b9faebfe8f1d62
Megan Dean <megandean937@gmail.com>
2019-09-12 19:48:17 -0500

    Controller Updates

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -2,11 +2,23 @@ class CarsController < ApplicationController
   def index
     @cars = Car.all
   end
+  def show
+    @car = Car.find(params[:id])
+  end
   def edit
     @car = Car.find(params[:id])
   end
   def update
     @car = Car.find(params[:id])
+    @car.update(car_params)
+    redirect_to @car
+  end
+  def new
+    @car = Car.new
+  end
+  def create
+    @car = Car.create(car_params)
+    redirect_to @car
   end
 
 
