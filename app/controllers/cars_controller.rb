31d9cd4fa201b910fb17c14143bd4b7eafaa1fe6
Megan Dean <megandean937@gmail.com>
2019-09-12 19:00:29 -0500

    Form

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,5 +1,17 @@
 class CarsController < ApplicationController
   def index
-    @cars = Car.all 
+    @cars = Car.all
   end
+  def edit
+    @car = Car.find(params[:id])
+  end
+  def update
+    @car = Car.find(params[:id])
+  end
+
+
+private
+def car_params
+  params.require(:car).permit(:make, :model, :year)
+end
 end
