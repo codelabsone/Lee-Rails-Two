eeab3557d7be3a9be6dfbf87fef392e534b1ffba
ndschalk <52681861+ndschalk@users.noreply.github.com>
2019-09-21 12:50:13 -0500

    validations

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -2,24 +2,35 @@ class CarsController < ApplicationController
   def index
     @cars = Car.all
   end
+
   def show
     @car = Car.find(params[:id])
   end
+
   def edit
     @car = Car.find(params[:id])
     @car.update(car_params)
   end
+
   def update
     @car = Car.find(params[:id])
     @car.update(car_params)
-    redirect_to @car
+    if @car.save
+      redirect_to @car
+    else car.update
+      render :edit
   end
+
   def new
     @car = Car.new
   end
+
   def create
     @car = Car.create(car_params)
-    redirect_to @car
+    if @car.save
+      redirect_to @car
+    else
+      render :new
   end
 
 
--- app/models/car.rb
+++ app/models/car.rb
@@ -1,2 +1,9 @@
 class Car < ApplicationRecord
+
+  validates :model, :make, :year, :color, :milage, presence: true
+
+  validates :description, length:{minimum: 35}
+
+  validates :price, numericality:{only_interger: true, greater_than: 5000}
+
 end
