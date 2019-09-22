dce5d14cf513996e0e0e9f1095008bbf102df1bb
Kayla Barton <kdbarton1s@gmail.com>
2019-09-21 20:49:42 -0500

    Added bootstrap formatting

--- app/helpers/cars_helper.rb
+++ app/helpers/cars_helper.rb
@@ -1,2 +1,19 @@
 module CarsHelper
+
+  def image_for(car)
+    if car.image_file_name.blank?
+      image_tag("placeholder.jpg")
+    else
+      image_tag(car.image_file_name)
+    end
+  end
+
+  def price_for(car)
+    if car.price.blank?
+      "Call for price."
+    else
+      number_to_currency(car.price)
+    end  
+  end
+
 end
