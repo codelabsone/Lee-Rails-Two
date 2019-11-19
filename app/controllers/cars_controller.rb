8257423bca55977cab08ef84ccef678ec6dd8bdf
Kayla <kdbarton1s@gmail.com>
2019-11-19 15:13:23 -0600

    All links on header now working.

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,6 +1,23 @@
 class CarsController < ApplicationController
   def index
-    @cars = Car.all
+    case params[:scope]
+      when 'sedan'
+        @cars = Car.sedan
+      when 'suv'
+        @cars = Car.suv
+      when 'hatchback'
+        @cars = Car.hatchback
+      when 'coupe'
+        @cars = Car.coupe
+      when 'hybrid'
+        @cars = Car.hybrid
+      when 'recent'
+        @cars = Car.recent
+      when 'used'
+        @cars = Car.used
+      else
+        @cars = Car.all
+    end
   end
 
   def show
@@ -29,6 +46,6 @@ class CarsController < ApplicationController
 
 private
 def car_params
-  params.require(:car).permit(:make, :model, :year, :price, :color, :description, :mileage, :image_file_name)
+  params.require(:car).permit(:make, :model, :year, :price, :color, :description, :mileage, :image_file_name, :type)
 end
 end
--- app/models/car.rb
+++ app/models/car.rb
@@ -1,11 +1,27 @@
 class Car < ApplicationRecord
+  self.inheritance_column = nil
   has_many :reviews, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :fans, through: :favorites, source: :user
-
+  TYPES = %w(Sedan SUV Hatchback Coupe Hybrid)
   # def to_param
   # "#{id}-#{make.parameterize}"
   # end
+  scope :recent, ->{ where('mileage < 1000') }
+
+  scope :used, ->{ where('mileage >= 1000') }
+
+  scope :sedan, ->{ where(type: 'Sedan')}
+
+  scope :suv, ->{ where(type: 'SUV') }
+
+  scope :hatchback, ->{ where(type: 'Hatchback') }
+
+  scope :coupe, ->{ where(type: 'Coupe') }
+
+  scope :hybrid, ->{ where(type: 'Hybrid') }
+
+  # scope :
 
   def self.recently_added
   order('created_at desc').limit(3)
