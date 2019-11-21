a9e2575a384389a1a44f2b549d2c13c8f622bbc4
Megan Dean <megandean937@gmail.com>
2019-11-21 12:45:41 -0600

    Fixed reviews controller and new page

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,29 +2,30 @@ class ReviewsController < ApplicationController
   before_action :require_signin
   before_action :set_car
 
-def index
-  @reviews = @car.reviews
-end
-
-def new
-  @review = @car.reviews.new
-end
-
-def create
-  @review = @car.reviews.new(review_params)
-  @review.user = current_user
-  if @review.save
-    redirect_to_car_reviews_path(@car), notice: "Thank you for reviewing."
-  else
-    render :new
+  def index
+    @reviews = @car.reviews
   end
-end
-end
 
-def destroy
-  @review = @car.reviews.find(params[:id])
-  @review.destroy
-  redirect_to_car_reviews_path(@car), notice: "Review removed!"
+  def new
+    @review = @car.reviews.new
+  end
+
+  def create
+    @review = @car.reviews.new(review_params)
+    @review.user = current_user
+    if @review.save
+      redirect_to car_reviews_path(@car), notice: "Thank you for reviewing."
+    else
+      render :new
+    end
+  end
+
+
+  def destroy
+    @review = @car.reviews.find(params[:id])
+    @review.destroy
+    redirect_to car_reviews_path(@car), notice: "Review removed!"
+  end
 end
 
 
