a39cc8e0c6a3cf90e11ca6755f98e5f0f1b205bd
Kayla <kdbarton1s@gmail.com>
2019-11-21 15:35:51 -0600

    Update seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,11 +2,89 @@ Car.create!([
   {
     make: "Honda",
     model: "Civic",
-    year: 2018
+    year: 2019,
+    color: "blue",
+    mileage: 20,
+    description: "There we are. In nature, dead trees are just as normal as live trees. Here's another little happy bush It's beautiful - and we haven't even done anything to it yet. You can bend rivers. But when I get home, the only thing I have power over is the garbage. By now you should be quite happy about what's happening here.",
+    image_file_name: "blue_civic_2019.jpg",
+    price: 20000,
+    type: "Sedan"
   },
   {
     make: "Honda",
     model: "Civic",
-    year: 2019
+    year: 2019,
+    type: "Hatchback",
+    color: "grey",
+    mileage: 100,
+    description: "There we are. In nature, dead trees are just as normal as live trees. Here's another little happy bush It's beautiful - and we haven't even done anything to it yet. You can bend rivers. But when I get home, the only thing I have power over is the garbage. By now you should be quite happy about what's happening here.",
+    image_file_name: "civic_hb_gray_2019.jpg",
+    price: 22000
+  },
+  {
+    make: "Honda",
+    model: "CR-V",
+    year: 2019,
+    type: "SUV",
+    color: "red",
+    mileage: 100,
+    description: "There we are. In nature, dead trees are just as normal as live trees. Here's another little happy bush It's beautiful - and we haven't even done anything to it yet. You can bend rivers. But when I get home, the only thing I have power over is the garbage. By now you should be quite happy about what's happening here.",
+    image_file_name: "crv_red_2019.jpg",
+    price: 25000
+  },
+  {
+    make: "Honda",
+    model: "Accord",
+    year: 2019,
+    type: "Sedan",
+    color: "grey",
+    mileage: 50,
+    description: "There we are. In nature, dead trees are just as normal as live trees. Here's another little happy bush It's beautiful - and we haven't even done anything to it yet. You can bend rivers. But when I get home, the only thing I have power over is the garbage. By now you should be quite happy about what's happening here.",
+    image_file_name: "accord_sedan.jpg",
+    price: 23000
+  },
+  {
+    make: "Honda",
+    model: "Insight",
+    year: 2019,
+    type: "Sedan",
+    color: "pearl",
+    mileage: 20,
+    description: "There we are. In nature, dead trees are just as normal as live trees. Here's another little happy bush It's beautiful - and we haven't even done anything to it yet. You can bend rivers. But when I get home, the only thing I have power over is the garbage. By now you should be quite happy about what's happening here.",
+    image_file_name: "insight_pearl_2019.jpg",
+    price: 23000
+  },
+  {
+    make: "Honda",
+    model: "Civic",
+    year: 2019,
+    type: "Coupe",
+    color: "white",
+    mileage: 150,
+    description: "There we are. In nature, dead trees are just as normal as live trees. Here's another little happy bush It's beautiful - and we haven't even done anything to it yet. You can bend rivers. But when I get home, the only thing I have power over is the garbage. By now you should be quite happy about what's happening here.",
+    image_file_name: "civic_coupe_white_2019.jpg",
+    price: 21000
+  },
+  {
+    make: "Honda",
+    model: "Clarity",
+    year: 2019,
+    type: "Hybrid",
+    color: "red",
+    mileage: 20,
+    description: "There we are. In nature, dead trees are just as normal as live trees. Here's another little happy bush It's beautiful - and we haven't even done anything to it yet. You can bend rivers. But when I get home, the only thing I have power over is the garbage. By now you should be quite happy about what's happening here.",
+    image_file_name: "clarity_red_2019.jpg",
+    price: 34000
+  },
+  {
+    make: "Honda",
+    model: "Civic",
+    year: 2017,
+    type: "Sedan",
+    color: "white",
+    mileage: 30000,
+    description: "There we are. In nature, dead trees are just as normal as live trees. Here's another little happy bush It's beautiful - and we haven't even done anything to it yet. You can bend rivers. But when I get home, the only thing I have power over is the garbage. By now you should be quite happy about what's happening here.",
+    image_file_name: "civic_white_2017.jpg",
+    price: 17000
   }
 ])
